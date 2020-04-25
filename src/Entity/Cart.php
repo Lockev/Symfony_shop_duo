31a9dc4ecee71bc0699b9710d414af0bad92fe56
Guillaume PRIGENT <guillaume.prigent92@gmail.com>
2020-04-25 22:13:31 +0200

    Added asserts + errors (still bugged)

--- src/Entity/Cart.php
+++ src/Entity/Cart.php
@@ -5,115 +5,116 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CartRepository")
  */
 class Cart
 {
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+  /**
+   * @ORM\Id()
+   * @ORM\GeneratedValue()
+   * @ORM\Column(type="integer")
+   */
+  private $id;
 
-    /**
-     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="carts")
-     * @ORM\JoinColumn(nullable=false)
-     */
-    private $user;
+  /**
+   * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="carts")
+   * @ORM\JoinColumn(nullable=false)
+   */
+  private $user;
 
-    /**
-     * @ORM\Column(type="datetime", nullable=true)
-     */
-    private $purchaseDate;
+  /**
+   * @ORM\Column(type="datetime", nullable=true)
+   */
+  private $purchaseDate;
 
-    /**
-     * @ORM\Column(type="boolean")
-     */
-    private $state = false;
+  /**
+   * @ORM\Column(type="boolean")
+   */
+  private $state = false;
 
-    /**
-     * @ORM\OneToMany(targetEntity="App\Entity\CartContent", mappedBy="cart", orphanRemoval=true, fetch="EAGER")
-     */
-    private $cartContents;
+  /**
+   * @ORM\OneToMany(targetEntity="App\Entity\CartContent", mappedBy="cart", orphanRemoval=true, fetch="EAGER")
+   */
+  private $cartContents;
 
-    public function __construct($user)
-    {
-        $this->cartContents = new ArrayCollection();
-        $this->setUser($user);
+  public function __construct($user)
+  {
+    $this->cartContents = new ArrayCollection();
+    $this->setUser($user);
+  }
+
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
+
+  public function getUser(): ?User
+  {
+    return $this->user;
+  }
+
+  public function setUser(?User $user): self
+  {
+    $this->user = $user;
+
+    return $this;
+  }
+
+  public function getPurchaseDate(): ?\DateTimeInterface
+  {
+    return $this->purchaseDate;
+  }
+
+  public function setPurchaseDate(\DateTimeInterface $purchaseDate): self
+  {
+    $this->purchaseDate = $purchaseDate;
+
+    return $this;
+  }
+
+  public function getState(): ?bool
+  {
+    return $this->state;
+  }
+
+  public function setState(bool $state): self
+  {
+    $this->state = $state;
+
+    return $this;
+  }
+
+  /**
+   * @return Collection|CartContent[]
+   */
+  public function getCartContents(): Collection
+  {
+    return $this->cartContents;
+  }
+
+  public function addCartContent(CartContent $cartContent): self
+  {
+    if (!$this->cartContents->contains($cartContent)) {
+      $this->cartContents[] = $cartContent;
+      $cartContent->setCart($this);
     }
 
-    public function getId(): ?int
-    {
-        return $this->id;
+    return $this;
+  }
+
+  public function removeCartContent(CartContent $cartContent): self
+  {
+    if ($this->cartContents->contains($cartContent)) {
+      $this->cartContents->removeElement($cartContent);
+      // set the owning side to null (unless already changed)
+      if ($cartContent->getCart() === $this) {
+        $cartContent->setCart(null);
+      }
     }
 
-    public function getUser(): ?User
-    {
-        return $this->user;
-    }
-
-    public function setUser(?User $user): self
-    {
-        $this->user = $user;
-
-        return $this;
-    }
-
-    public function getPurchaseDate(): ?\DateTimeInterface
-    {
-        return $this->purchaseDate;
-    }
-
-    public function setPurchaseDate(\DateTimeInterface $purchaseDate): self
-    {
-        $this->purchaseDate = $purchaseDate;
-
-        return $this;
-    }
-
-    public function getState(): ?bool
-    {
-        return $this->state;
-    }
-
-    public function setState(bool $state): self
-    {
-        $this->state = $state;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|CartContent[]
-     */
-    public function getCartContents(): Collection
-    {
-        return $this->cartContents;
-    }
-
-    public function addCartContent(CartContent $cartContent): self
-    {
-        if (!$this->cartContents->contains($cartContent)) {
-            $this->cartContents[] = $cartContent;
-            $cartContent->setCart($this);
-        }
-
-        return $this;
-    }
-
-    public function removeCartContent(CartContent $cartContent): self
-    {
-        if ($this->cartContents->contains($cartContent)) {
-            $this->cartContents->removeElement($cartContent);
-            // set the owning side to null (unless already changed)
-            if ($cartContent->getCart() === $this) {
-                $cartContent->setCart(null);
-            }
-        }
-
-        return $this;
-    }
+    return $this;
+  }
 }
--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -5,147 +5,161 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
  */
 class Product
 {
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+  /**
+   * @ORM\Id()
+   * @ORM\GeneratedValue()
+   * @ORM\Column(type="integer")
+   */
+  private $id;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $name;
+  /**
+   * @ORM\Column(type="string", length=255)
+   * @Assert\NotNull
+   * @Assert\Length(
+   *      min = 1,
+   *      max = 255
+   * )
+   */
+  private $name;
 
-    /**
-     * @ORM\Column(type="text")
-     */
-    private $description;
+  /**
+   * @ORM\Column(type="text")
+   * @Assert\NotNull
+   */
+  private $description;
 
-    /**
-     * @ORM\Column(type="float")
-     */
-    private $price;
+  /**
+   * @ORM\Column(type="float")
+   * @Assert\NotNull
+   * @Assert\Type("float")
+   * @Assert\GreaterThanOrEqual(0)
+   */
+  private $price;
 
-    /**
-     * @ORM\Column(type="integer")
-     */
-    private $stock;
+  /**
+   * @ORM\Column(type="integer")
+   * @Assert\NotNull
+   * @Assert\Type("integer")
+   * @Assert\GreaterThanOrEqual(0)
+   */
+  private $stock;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $picture;
+  /**
+   * @ORM\Column(type="string", length=255)
+   * @Assert\NotNull
+   */
+  private $picture;
 
-    /**
-     * @ORM\OneToMany(targetEntity="App\Entity\CartContent", mappedBy="product", orphanRemoval=true)
-     */
-    private $cartContents;
+  /**
+   * @ORM\OneToMany(targetEntity="App\Entity\CartContent", mappedBy="product", orphanRemoval=true)
+   */
+  private $cartContents;
 
-    public function __construct()
-    {
-        $this->cartContents = new ArrayCollection();
+  public function __construct()
+  {
+    $this->cartContents = new ArrayCollection();
+  }
+
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
+
+  public function getName(): ?string
+  {
+    return $this->name;
+  }
+
+  public function setName(string $name): self
+  {
+    $this->name = $name;
+
+    return $this;
+  }
+
+  public function getDescription(): ?string
+  {
+    return $this->description;
+  }
+
+  public function setDescription(string $description): self
+  {
+    $this->description = $description;
+
+    return $this;
+  }
+
+  public function getPrice(): ?float
+  {
+    return $this->price;
+  }
+
+  public function setPrice(float $price): self
+  {
+    $this->price = $price;
+
+    return $this;
+  }
+
+  public function getStock(): ?int
+  {
+    return $this->stock;
+  }
+
+  public function setStock(int $stock): self
+  {
+    $this->stock = $stock;
+
+    return $this;
+  }
+
+  public function getPicture(): ?string
+  {
+    return $this->picture;
+  }
+
+  public function setPicture(string $picture): self
+  {
+    $this->picture = $picture;
+
+    return $this;
+  }
+
+  /**
+   * @return Collection|CartContent[]
+   */
+  public function getCartContents(): Collection
+  {
+    return $this->cartContents;
+  }
+
+  public function addCartContent(CartContent $cartContent): self
+  {
+    if (!$this->cartContents->contains($cartContent)) {
+      $this->cartContents[] = $cartContent;
+      $cartContent->setProduct($this);
     }
 
-    public function getId(): ?int
-    {
-        return $this->id;
+    return $this;
+  }
+
+  public function removeCartContent(CartContent $cartContent): self
+  {
+    if ($this->cartContents->contains($cartContent)) {
+      $this->cartContents->removeElement($cartContent);
+      // set the owning side to null (unless already changed)
+      if ($cartContent->getProduct() === $this) {
+        $cartContent->setProduct(null);
+      }
     }
 
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
-
-    public function setDescription(string $description): self
-    {
-        $this->description = $description;
-
-        return $this;
-    }
-
-    public function getPrice(): ?float
-    {
-        return $this->price;
-    }
-
-    public function setPrice(float $price): self
-    {
-        $this->price = $price;
-
-        return $this;
-    }
-
-    public function getStock(): ?int
-    {
-        return $this->stock;
-    }
-
-    public function setStock(int $stock): self
-    {
-        $this->stock = $stock;
-
-        return $this;
-    }
-
-    public function getPicture(): ?string
-    {
-        return $this->picture;
-    }
-
-    public function setPicture(string $picture): self
-    {
-        $this->picture = $picture;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|CartContent[]
-     */
-    public function getCartContents(): Collection
-    {
-        return $this->cartContents;
-    }
-
-    public function addCartContent(CartContent $cartContent): self
-    {
-        if (!$this->cartContents->contains($cartContent)) {
-            $this->cartContents[] = $cartContent;
-            $cartContent->setProduct($this);
-        }
-
-        return $this;
-    }
-
-    public function removeCartContent(CartContent $cartContent): self
-    {
-        if ($this->cartContents->contains($cartContent)) {
-            $this->cartContents->removeElement($cartContent);
-            // set the owning side to null (unless already changed)
-            if ($cartContent->getProduct() === $this) {
-                $cartContent->setProduct(null);
-            }
-        }
-
-        return $this;
-    }
+    return $this;
+  }
 }
