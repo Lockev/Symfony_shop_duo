18246f0f897c4d79b9e031256f9fc194d168ed46
Guillaume PRIGENT <guillaume.prigent92@gmail.com>
2020-04-26 11:16:25 +0200

    Cart buy + trad + my cart

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -4,20 +4,42 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use App\Entity\Cart;
 
 /**
  *  @Route("/{_locale}")
  */
 class CartController extends AbstractController
 {
-    /**
-     * @Route("/cart", name="cart")
-     */
-    public function index()
-    {
-        $cartContents = $this->getUser()->getActualCart()->getCartContents();
-        return $this->render('cart/index.html.twig', [
-            'cartContents' => $cartContents,
-        ]);
-    }
+  /**
+   * @Route("/cart", name="cart")
+   * 
+   */
+  public function index()
+  {
+    $cartContents = $this->getUser()->getActualCart()->getCartContents();
+    return $this->render('cart/index.html.twig', [
+      'cartContents' => $cartContents,
+    ]);
+  }
+
+  /**
+   * @Route("/cart/buy", name="buy_cart")
+   * 
+   */
+  public function buy(TranslatorInterface $translator)
+  {
+    $em = $this->getDoctrine()->getManager();
+    $currentCart = $this->getUser()->getActualCart()->setState(true);
+    $cart = new Cart($this->getUser());
+    $em->persist($currentCart);
+    $em->persist($cart);
+    $em->flush();
+
+    $this->addFlash('success', $translator->trans('flash.success.cartPurchased'));
+
+    return $this->redirectToRoute('product');
+  }
 }
